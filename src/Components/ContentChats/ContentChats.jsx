3dc8426f21cec115164426ac253623ce458c29af
Manuel <manuelusero6@gmail.com>
2024-07-28 23:59:29 -0300

    modifique estilos

--- src/Components/ContentChats/ContentChats.jsx
+++ src/Components/ContentChats/ContentChats.jsx
@@ -4,24 +4,24 @@ import { Link } from "react-router-dom";
 
 const ContentChats = ({ nombre, thumbnail }) => {
   return (
-    <>
-      {/* <div>{nombre}</div> */}
-      <div className="content-chats">
-        <div className="info-contact">
-          <Link to="/">
-            <i className="bi bi-arrow-left"></i>
-          </Link>
-          <div className="img-container">
-            <img src={thumbnail} alt="user-pic" className="user-pic" />
-          </div>
-          <span className="user-name">{nombre}</span>
-          <div className="icons">
-            <i className="bi bi-telephone"></i>
-            <i className="bi bi-camera-video"></i>
-          </div>
+
+    <div id="chats-container" className="content-chats">
+      <div className="info-contact">
+        <Link to="/">
+          <i className="bi bi-arrow-left"></i>
+        </Link>
+        <div className="img-container">
+          <img src={thumbnail} alt="user-pic" className="user-pic" />
+        </div>
+        <span className="user-name">{nombre}</span>
+        <div className="icons">
+          <i className="bi bi-camera-video"></i>
+          <i className="bi bi-telephone"></i>
+          <i className="bi bi-three-dots-vertical"></i>
         </div>
       </div>
-    </>
+    </div>
+
   );
 };
 
