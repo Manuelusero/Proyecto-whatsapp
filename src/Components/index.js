7ed177200e05f8a4fc82080b3a37a948d3dc839f
Manuel <manuelusero6@gmail.com>
2024-08-20 10:55:44 -0300

    Terminando errores

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,6 +1,5 @@
-import ListContact from "./List-contact/ListContact";
+import ListContact from "./list-contact/ListContact";
 import ContentChats from "./ContentChats/ContentChats";
-import MessageForm from "./MessageForm/MessageForm";
 // import Contact from "/Contact/Contact";
 
-export { ContentChats, ListContact };
+export { ContentChats, ListContact};
